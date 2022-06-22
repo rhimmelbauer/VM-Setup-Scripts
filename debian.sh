9b743b94c111d236d33e0613df9e415b704e38eb
Roberto Himmelbauer <32716015+rhimmelbauer@users.noreply.github.com>
2022-06-21 20:00:55 -0500

    added more usefull cmds

--- debian.sh
+++ debian.sh
@@ -1,4 +1,17 @@
 
 sudo apt update && sudo apt upgrade
-sudo apt install build-essential lib32z1
+sudo apt install wget build-essential libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 sudo apt install git
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install python3-venv
+
+ssh-keygen -t ed25519
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+
+python3 -m venv venv
+
+pip3 install -U pip
+pip3 install -U wheel
+pip3 install -U setuptools
